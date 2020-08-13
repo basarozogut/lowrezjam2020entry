6bfc4650ab39dc94d6a54ce84e3532d15ad25db3
chillwaves <5684602+chillwaves@users.noreply.github.com>
2020-08-13 22:59:31 +0300

    ending.

--- source/EndgameState.hx
+++ source/EndgameState.hx
@@ -1,13 +1,11 @@
 package;
 
 import flixel.FlxG;
-import flixel.FlxSprite;
 import flixel.FlxState;
 import flixel.text.FlxText;
 import flixel.tweens.FlxEase;
 import flixel.tweens.FlxTween;
 import flixel.util.FlxColor;
-import flixel.util.FlxTimer.FlxTimerManager;
 import flixel.util.FlxTimer;
 import openfl.Lib;
 
@@ -16,38 +14,63 @@ class EndgameState extends FlxState
 	var _text:Array<String> = [
 		"\n\n\nGAME\nCOMPLETED!",
 		"\n\n\nFINAL SCORE\n" + LevelManager.instance.getScore().getAmountText(),
-		"\n\nGFX SFX\nMUSIC\nCODING\n\nCHILLWAVES",
-		"\n8 BIT WONDER\nFONT by\n\nJoiro Hatagaya",
+		"\n\nGFX\nMUSIC\nCODING\n\nCHILLWAVES",
+		"\n\nSFX MADE\nWITH SFXR\n\nBY\nTOMAS PETTERSSON",
+		"\n\n8 BIT WONDER\nFONT by\n\nJoiro Hatagaya",
 		"\n\n\nTHANKS FOR\nPLAYING",
 		"\n\n\nPRESS ANY KEY\nTO EXIT"
 	];
 	var _currentTextIndex:Int;
 	var _levelCompleteText:FlxText;
 	var _waitTimer:FlxTimer;
+	var _canQuit:Bool;
+
+	var _coins:Array<Coin>;
 
 	override public function create()
 	{
 		super.create();
 
+		_canQuit = false;
 		_currentTextIndex = 0;
 		_waitTimer = new FlxTimer(FlxTimer.globalManager);
 
+		var nCoins = 10;
+		_coins = new Array<Coin>();
+		for (i in 0...nCoins)
+		{
+			var coin = new Coin();
+			coin.x = FlxG.width * i / nCoins;
+			coin.y = FlxG.random.float(-FlxG.height, -coin.height);
+			coin.velocity.y = 5 + FlxG.random.float(35);
+			_coins.push(coin);
+			add(coin);
+		}
+
 		_levelCompleteText = new FlxText(0, 0, FlxG.width);
 		_levelCompleteText.y = 0;
-		_levelCompleteText.setFormat(FontManager.instance.getScoreFont(), 6, FlxColor.WHITE, FlxTextAlign.CENTER);
+		_levelCompleteText.setFormat(FontManager.instance.getScoreFont(), 6, FlxColor.WHITE, FlxTextAlign.CENTER, SHADOW, 0x33000000);
 		add(_levelCompleteText);
 
 		FlxG.sound.playMusic(AssetPaths.lowrezjam2020_credits__ogg, .5, true);
-
 		nextText();
 	}
 
 	override public function update(elapsed:Float)
 	{
-		if (FlxG.keys.justPressed.ANY)
+		if (FlxG.keys.justPressed.ANY && _canQuit)
 		{
 			Sys.exit(0);
 		}
+
+		for (coin in _coins)
+		{
+			if (coin.y > FlxG.height)
+			{
+				coin.y = -coin.height;
+			}
+		}
+
 		super.update(elapsed);
 	}
 
@@ -57,7 +80,13 @@ class EndgameState extends FlxState
 		var text = _text[_currentTextIndex];
 		_levelCompleteText.text = text;
 
-		_currentTextIndex = (_currentTextIndex + 1) % _text.length;
+		_currentTextIndex++;
+		if (_currentTextIndex >= _text.length)
+		{
+			_currentTextIndex = 0;
+			_canQuit = true;
+		}
+
 		FlxTween.tween(_levelCompleteText, {x: 0}, 1, {
 			type: FlxTweenType.ONESHOT,
 			ease: FlxEase.cubeInOut,
