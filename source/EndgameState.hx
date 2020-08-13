8995f05ff78683a75b762289d57aa9e38f4e8bd3
chillwaves <5684602+chillwaves@users.noreply.github.com>
2020-08-13 19:03:05 +0300

    end screen tweaks.

--- source/EndgameState.hx
+++ source/EndgameState.hx
@@ -1,7 +1,13 @@
 package;
 
+import flixel.FlxG;
+import flixel.FlxSprite;
 import flixel.FlxState;
 import flixel.text.FlxText;
+import flixel.tweens.FlxEase;
+import flixel.tweens.FlxTween;
+import flixel.util.FlxColor;
+import openfl.Lib;
 
 class EndgameState extends FlxState
 {
@@ -9,7 +15,21 @@ class EndgameState extends FlxState
 	{
 		super.create();
 
-		var endText = new FlxText(0, 0, "THE END");
-		add(endText);
+		var levelCompleteText = new FlxText(0, 0, FlxG.width, "GAME COMPLETED!\n\nA GAME BY\nCHILLWAVES\n\nTHANKS FOR PLAYING");
+		levelCompleteText.x = -FlxG.width;
+		levelCompleteText.y = 4;
+		levelCompleteText.setFormat(FontManager.instance.getScoreFont(), 6, FlxColor.WHITE, FlxTextAlign.CENTER);
+		add(levelCompleteText);
+
+		FlxTween.tween(levelCompleteText, {x: 0}, .7, {type: FlxTweenType.ONESHOT, ease: FlxEase.cubeInOut});
+	}
+
+	override public function update(elapsed:Float)
+	{
+		if (FlxG.keys.justPressed.ANY)
+		{
+			Sys.exit(0);
+		}
+		super.update(elapsed);
 	}
 }
